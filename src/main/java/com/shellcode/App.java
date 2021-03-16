83940c68e8907c2f58dc4b790a559674db0fdd6a
Maciej Wołosewicz <maciej.wolosewicz@gmail.com>
2021-03-16 20:46:15 +0100

    All log4j2 example configuration.

--- src/main/java/com/shellcode/App.java
+++ src/main/java/com/shellcode/App.java
@@ -1,14 +1,25 @@
 package com.shellcode;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
+    static Logger log = LoggerFactory.getLogger(App.class.getName());
+
     public static void main( String[] args )
     {
+        log.info("Info debug");
+        log.error("Error debug");
+        log.debug("Debug debug");
+        log.warn("Warning debug");
+        log.trace("Trace debug");
 
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" +  " class name: " + App.class.getName() );
     }
 }
