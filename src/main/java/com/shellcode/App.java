85b1732000f453958f400ab3b48aeff7fa3aa8ca
Maciej Wołosewicz <maciej.wolosewicz@gmail.com>
2021-03-16 22:17:57 +0100

    Add working hibernate example.

--- src/main/java/com/shellcode/App.java
+++ src/main/java/com/shellcode/App.java
@@ -1,7 +1,13 @@
 package com.shellcode;
 
+import com.shellcode.db.HibernateUtil;
+import com.shellcode.db.Unit;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 /**
  * Hello world!
@@ -20,6 +26,36 @@ public class App
         log.warn("Warning debug");
         log.trace("Trace debug");
 
-        System.out.println( "Hello World!" +  " class name: " + App.class.getName() );
+        Unit celsius = new Unit("C", "Temperature.");
+        Unit pascal = new Unit("Pa", "One newton per square metre");
+
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student objects
+            session.save(celsius);
+            session.save(pascal);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            List< Unit > units = session.createQuery("from Unit", Unit.class).list();
+            units.forEach(s -> log.info("Unit: " + s.getUnit() + " desc: " + s.getDescription() + ", id: " + s.getId()));
+        } catch (Exception e) {
+            log.error("Can't get data: " + e.getMessage());
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+
+
     }
 }
--- src/main/java/com/shellcode/db/Unit.java
+++ src/main/java/com/shellcode/db/Unit.java
@@ -1,11 +1,30 @@
-package com.shellcode.base;
+package com.shellcode.db;
+
+import javax.persistence.*;
 
 /**
  * Class represents unit. It should be used to descrive measurement value.
  */
+@Entity
+@Table(name = "si_units")
 public class Unit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "si_unit")
     private String unit;
+
+    @Column(name = "unit_desc")
     private String description;
+
+    /**
+     * Default constructor.
+     */
+    public Unit() {
+    }
 
     /**
      * Constructor.
@@ -16,6 +35,14 @@ public class Unit {
     public Unit(String unit, String description) {
         this.unit = unit;
         this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getUnit() {
@@ -32,5 +59,10 @@ public class Unit {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "SI unit: " + unit + ", is: " + description;
     }
 }
