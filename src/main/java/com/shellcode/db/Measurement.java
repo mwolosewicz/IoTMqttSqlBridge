0ec5aeed2275fec93f30b56b069d6a481bf98ef6
Maciej Wołosewicz <maciej.wolosewicz@gmail.com>
2021-03-17 20:46:03 +0100

    Add measurement to persist.

--- src/main/java/com/shellcode/db/Measurement.java
+++ src/main/java/com/shellcode/db/Measurement.java
@@ -1,13 +1,28 @@
 package com.shellcode.db;
 
+import javax.persistence.*;
 import java.sql.Timestamp;
 
 /**
  * Class represents single measurement.
  */
+@Entity
+@Table(name = "measurements")
 public class Measurement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "timestamp")
     private Timestamp timestamp;
+
+    @Column(name = "value")
     private Double value;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "si_unit", referencedColumnName = "id")
     private Unit unit;
 
     /**
@@ -27,6 +42,14 @@ public class Measurement {
         this.timestamp = timestamp;
         this.value = value;
         this.unit = unit;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public Timestamp getTimestamp() {
@@ -51,5 +74,10 @@ public class Measurement {
 
     public void setUnit(Unit unit) {
         this.unit = unit;
+    }
+
+    @Override
+    public String toString() {
+        return "Measurement[ id: " + id + " value: " + value + unit.getUnit() + "]";
     }
 }
